0d25b13c744e4780b7a55a0499fc566c3bf976eb
MatveevaLI <ladaivanna.matveeva@gmail.com>
2024-03-02 16:20:24 +0100

    change pacient name

--- src/components/matveevali-ambulance-wl-list/matveevali-ambulance-wl-list.tsx
+++ src/components/matveevali-ambulance-wl-list/matveevali-ambulance-wl-list.tsx
@@ -12,7 +12,7 @@ export class MatveevaliAmbulanceWlList {
   private async getWaitingPatientsAsync() {
     return await Promise.resolve(
       [{
-        name: 'Jožko Púčik',
+        name: 'Jožko Púčikaka',
         patientId: '10001',
         since: new Date(Date.now() - 10 * 60).toISOString(),
         estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
